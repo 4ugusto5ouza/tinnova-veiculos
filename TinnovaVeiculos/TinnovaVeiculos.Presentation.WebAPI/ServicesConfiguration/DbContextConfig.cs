6c3c8ac4e872327595e90f08523e589aa3932c41
Samuel Souza <augusto_souza@edu.unifil.br>
2022-09-14 09:27:55 -0300

    :sparkles:feat: add dependencias ef core e iniciado sql server in startup class para code first

---
+++ TinnovaVeiculos/TinnovaVeiculos.Presentation.WebAPI/ServicesConfiguration/DbContextConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TinnovaVeiculos.Infrastruture.Data.Contexts;
+
+namespace TinnovaVeiculos.Presentation.WebAPI.ServicesConfiguration
+{
+    public static class DbContextConfig
+    {
+        public static readonly ILoggerFactory TinovvaVeiculosDBLoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        public static void AddDbContextConfiguration(this IServiceCollection services, IConfiguration config)
+        {
+
+            services.AddDbContext<TinnovaVeiculosDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+                options.UseLoggerFactory(TinovvaVeiculosDBLoggerFactory);
+                //options.EnableSensitiveDataLogging();
+                //options.EnableDetailedErrors();
+            });
+        }
+    }
+}
