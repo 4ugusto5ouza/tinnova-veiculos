e12d63d2de796e153f66e696926d9bc6bbb5e4a3
Samuel Souza <augusto_souza@edu.unifil.br>
2022-09-14 09:02:05 -0300

    :sparkles:feat: criado e  organizado projeto e implementado classe veiculo

---
+++ TinnovaVeiculos/TinnovaVeiculos.Domain/Entities/Veiculo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TinnovaVeiculos.Domain.Entities
+{
+    public class Veiculo
+    {
+        public Veiculo()
+        {
+            Vendido = false;
+            Created = DateTime.Now;
+        }
+
+        public Veiculo(string modelo, string marca, int ano, string descricao)
+        {
+            Modelo = modelo;
+            Marca = marca;
+            Ano = ano;
+            Descricao = descricao;
+            Vendido = false;
+            Created = DateTime.Now;
+        }
+
+        public long Id { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public string Descricao { get; set; }
+        public bool Vendido { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime? Updated { get; private set; }
+
+        public void SetVendido(bool vendido)
+        {
+            Vendido = vendido;
+        }
+
+        public void SetUpdated()
+        {
+            Updated = DateTime.Now;
+        }
+    }
+}
