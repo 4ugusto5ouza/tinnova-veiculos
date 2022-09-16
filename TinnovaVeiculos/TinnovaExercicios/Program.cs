b0c83b747d0f62e7e1fdb960fdbe99c722673ce8
Samuel Souza <augusto_souza@edu.unifil.br>
2022-09-15 23:44:09 -0300

    :sparkles:feat: Exercicios propotos

---
+++ TinnovaVeiculos/TinnovaExercicios/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TinnovaExercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultadoEleicao = new ResultadoEleicao(800, 150, 50);
+
+            Console.WriteLine(resultadoEleicao.CalcularPercentualVotosValidos() + "%");
+            Console.WriteLine(resultadoEleicao.CalcularPercentualVotosBrancos() + "%");
+            Console.WriteLine(resultadoEleicao.CalcularPercentualVotosNulos() + "%");
+
+            int[] vetor = { 5, 3, 2, 4, 7, 1, 0, 6 };
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i] + ", ");
+            }
+
+
+            int aux;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                for (int j = 0; j < vetor.Length - 1; j++)
+                {
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        aux = vetor[j + 1];
+                        vetor[j + 1] = vetor[j];
+                        vetor[j] = aux;
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i < vetor.Length; i++)
+                Console.Write(vetor[i] + ", ");
+
+            Console.WriteLine("");
+            Console.WriteLine(FatorialRecursao(6));
+
+            Console.Write("Digite um número: ");
+            Console.WriteLine(SomaMultiplosTresOuCinco(int.Parse(Console.ReadLine())));
+
+            Console.ReadKey();
+        }
+
+        public static int FatorialRecursao(int numero)
+        {
+            if (numero == 1)
+                return 1;
+            else
+                return numero * FatorialRecursao(numero - 1);
+        }
+
+        public static int SomaMultiplosTresOuCinco(int numero)
+        {
+            int resultado = 0;
+
+            for (int i = 0; i < numero; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    resultado += i;
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ TinnovaVeiculos/TinnovaExercicios/ResultadoEleicao.cs
@@ -0,0 +1,30 @@
+﻿namespace TinnovaExercicios
+{
+    public class ResultadoEleicao
+    {
+        public ResultadoEleicao(int votosValidos, int votosBrancos, int votosNulos)
+        {
+            VotosValidos = votosValidos;
+            VotosBrancos = votosBrancos;
+            VotosNulos = votosNulos;
+        }
+
+        public double VotosValidos { get; }
+        public double VotosBrancos { get; }
+        public double VotosNulos { get; }
+        public double TotalEleitores { get => VotosValidos + VotosBrancos + VotosNulos; }
+
+        public double CalcularPercentualVotosValidos()
+        {
+            return (VotosValidos / TotalEleitores) * 100;
+        }
+        public double CalcularPercentualVotosBrancos()
+        {
+            return (VotosBrancos / TotalEleitores) * 100;
+        }
+        public double CalcularPercentualVotosNulos()
+        {
+            return (VotosNulos / TotalEleitores) * 100;
+        }
+    }
+}
